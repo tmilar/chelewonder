defd80f7d06eb8bcbf9c7b8bbb7c4d1a45ad7753
Alberto Miranda <amiranda@elementum.com>
2018-11-07 20:00:14 -0300

    Fixed port

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const inject = require('./inject')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
