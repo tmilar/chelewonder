f4e258c9d309a457d4750ab9aa5c759d2a04532b
Alberto Miranda <amiranda@elementum.com>
2018-11-08 11:27:40 -0300

    Added current date highlighting.

--- inject.js
+++ inject.js
@@ -16,17 +16,27 @@ function inject () {
     display: flex;
     font: 20px arial;
   `
+  const currentDayIndex = new Date().getDay() - 1
+  const weekDays = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']
   const dropdowns = document.querySelectorAll('table select')
   const getLabel = index => dropdowns[index].selectedOptions[0].label
+  const getDayStyle = (index, isDessert) => {
+    if (isDessert) {
+      return index === currentDayIndex
+        ? 'background: #7b96e5; border-top: 1px solid white'
+        : 'background: #555; border-top: 1px solid #888'
+    }
 
-  const weekDays = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']
+    return index === currentDayIndex ? 'background: #5976c6' : 'background: #444'
+  }
+
   const modalContent = weekDays
     .map(
       (day, i) => `
-        <div style="flex: 1; background: #444; margin: 2px; display: flex; flex-direction: column; cursor: default">
+        <div style="flex: 1; margin: 2px; display: flex; flex-direction: column; cursor: default; ${getDayStyle(i)}">
           <div style="font-weight: bold; padding: 10px 5px;">${day}</div>
-          <div style="border-bottom: 1px solid #888; padding: 10px 5px; min-height: 100px;">${getLabel(i)}</div>
-          <div style="padding: 10px 5px; background: #555; flex: 1">${getLabel(i + 5)}</div>
+          <div style="padding: 10px 5px; min-height: 100px; flex: 1">${getLabel(i)}</div>
+          <div style="padding: 10px 5px; flex: 1; ${getDayStyle(i, true)}">${getLabel(i + 5)}</div>
         </div>
           `
     )
